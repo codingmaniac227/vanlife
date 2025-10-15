1540784c5852d4ecb82be045ca9691e3c9244b00
Coding <codingmaniac227@gmail.com>
2025-10-15 19:27:17 -0400

    feat(Home): finish Home page on the home route with styles included

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,11 +2,14 @@ import React, {useContext} from "react"
 import { Link } from "react-router-dom"
 import Button from '../components/Buttons'
 import { ButtonContext } from "../App";
+import '../styles/home.css'
+
+console.log("home rendered")
 
 export default function Home() {
 
     return (
-        <ButtonContext.Provider value={{orangeLongButton: 'orangeLongButton'}}>
+        <ButtonContext.Provider value={{ orangeLongButton: true }}>
             <div className="home-container">
                 <h1>You got the travel plans, we got the travel vans.</h1>
                 <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
