dc4ffaa42f532c7cd719371bc2f9f8836efc8d6a
Coding <codingmaniac227@gmail.com>
2025-10-15 16:34:34 -0400

    feat(Footer): initialize Footer component with styling

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            @ 2022 #VANLIFE
+        </footer>
+    );
+};
+
+export default Footer;
