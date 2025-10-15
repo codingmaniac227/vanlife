69d48ca9d6a2fcd02ebc7a68778ab138aa94ccb7
Coding <codingmaniac227@gmail.com>
2025-10-15 18:24:30 -0400

    refactor(Home): adjust Home page to serve context values and reusable components

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React, {useContext} from "react"
 import { Link } from "react-router-dom"
+import Button from '../components/Buttons'
+import { ButtonContext } from "../App";
 
 export default function Home() {
+
     return (
-        <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
-        </div>
+        <ButtonContext.Provider value={{orangeLongButton: 'orangeLongButton'}}>
+            <div className="home-container">
+                <h1>You got the travel plans, we got the travel vans.</h1>
+                <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+                <Button.Long><Link to="vans">Find your van</Link></Button.Long>
+            </div>
+        </ButtonContext.Provider>
     )
 };
