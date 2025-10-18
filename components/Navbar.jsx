a53435e331f9b3d5ee9a0845add3d4505d5bcdba
Coding <codingmaniac227@gmail.com>
2025-10-17 20:24:44 -0400

    style(Navbar): add classnames to each nav link for accessibility

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import '../styles/navbar.css'
 
 const Navbar = () => {
     return (
-        <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
-            </nav>
-        </header>
+        <>
+            <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+                <nav>
+                    <Link className='navbar-link' to="/host">Host</Link>
+                    <Link className='navbar-link' to="/about">About</Link>
+                    <Link className='navbar-link' to="/vans">Vans</Link>
+                </nav>
+            </header>
+        </>
     );
 };
 
