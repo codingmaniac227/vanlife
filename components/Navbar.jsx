aaad978a53667085de73b8ee532fc710fd48468f
Coding <codingmaniac227@gmail.com>
2025-10-24 07:59:28 -0400

    refactor(Navbar): implement NavLink library component instead of Link for appropriate use

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Link, Outlet} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import '../styles/navbar.css'
 
 const Navbar = () => {
+
+    const styles = { textDecoration: "underline", color: "#161616", fontWeight: '700', fontSize: '1.10rem' };
     return (
         <>
             <header>
                 <Link className="site-logo" to="/">#VanLife</Link>
-                <nav>
-                    <Link className='navbar-link' to="/host">Host</Link>
-                    <Link className='navbar-link' to="/about">About</Link>
-                    <Link className='navbar-link' to="/vans">Vans</Link>
+                <nav className="nav-links">
+                    <NavLink style={({isActive}) => isActive ? styles : null} to="/host">Host</NavLink>
+                    <NavLink style={({isActive}) => isActive ? styles : null} to="/about">About</NavLink>
+                    <NavLink style={({isActive}) => isActive ? styles : null} to="/vans">Vans</NavLink>
                 </nav>
             </header>
         </>
