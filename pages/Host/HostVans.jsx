ada4f0e75a99d5d3af6706c5c6650b6b74638db4
Coding <codingmaniac227@gmail.com>
2025-10-24 07:53:42 -0400

    chore(init): initialize hostvans and hostvanslisting pages

---
+++ pages/Host/HostVans.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+
+const HostVans = () => {
+
+
+    return (
+        <>
+            <section>
+                <h1>Your listed vans</h1>
+
+                <div>
+
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default HostVans;
---
+++ pages/Host/HostVansListing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+
+const HostVansListing = () => {
+
+    return (
+        <div>
+            HostVansListing
+        </div>
+    );
+};
+
+export default HostVansListing;
