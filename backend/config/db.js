df8bc5bac2137c6499f63beb0e5037a6052c00d2
Coding <codingmaniac227@gmail.com>
2025-10-24 16:16:32 -0400

    refactor(db): pool connection now links to Neon PG DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,9 @@
-import pkg from 'pg'
-import dotenv from 'dotenv'
-
-dotenv.config()
-const { Pool } = pkg
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
 export default pool;
