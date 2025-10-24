9568c487a1033413631940c1bedfb66499759932
Coding <codingmaniac227@gmail.com>
2025-10-24 08:12:18 -0400

    feat(server): entry point for backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: `http://localhost:${process.env.CORSORIGIN}`,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
